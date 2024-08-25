9edaa647832ea654708df74e866206bb9f0578bb
taka110811 <babysteps1108@gmail.com>
2024-08-25 12:13:01 +0900

    pytest setup teardown

--- test_calculation.py
+++ test_calculation.py
@@ -2,14 +2,31 @@ import pytest
 import calculation
 
 class TestCalTest():
+
+    @classmethod
+    def setup_class(cls):
+        print('start')
+        cls.cal = calculation.Cal()
+
+    @classmethod
+    def teardown_class(cls):
+        print('end')
+        del cls.cal
+
+    def setup_method(self, method):
+        print('method={}'.format(method.__name__))
+        # self.cal = calculation.Cal()
+
+    def teardown_method(self, method):
+        print('method={}'.format(method.__name__))
+        # del self.cal
+
     def test_add_num_and_double(self):
-        cal = calculation.Cal()
-        assert cal.add_num_and_double(1,1) == 4    
+        assert self.cal.add_num_and_double(1,1) == 4    
 
     def test_add_num_and_double_raise(self):
         with pytest.raises(ValueError):
-            cal = calculation.Cal()
-            cal.add_num_and_double('1','1')
+            self.cal.add_num_and_double('1','1')
 
 if __name__ == '__main__':
     pytest.main()
