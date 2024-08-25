c79fad09a32c732bf75f2b5815e47596d6d4f890
taka110811 <babysteps1108@gmail.com>
2024-08-25 11:58:59 +0900

    pytest class

--- test_calculation.py
+++ test_calculation.py
@@ -1,8 +1,9 @@
 import calculation
 
-def test_add_num_and_double():
-    cal = calculation.Cal()
-    assert cal.add_num_and_double(1,1) == 4 
+class TestCalTest():
+    def test_add_num_and_double(self):
+        cal = calculation.Cal()
+        assert cal.add_num_and_double(1,1) == 4    
 
 # release_name = 'lesson'
 
