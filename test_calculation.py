861acdac401f6b1aae981ba8c6834c9e9c0e0acb
taka110811 <babysteps1108@gmail.com>
2024-08-25 11:52:26 +0900

    unittest skip

--- test_calculation.py
+++ test_calculation.py
@@ -2,6 +2,8 @@ import unittest
 
 import calculation
 
+release_name = 'lesson'
+
 class CalTest(unittest.TestCase):
     def setUp(self):
         print('setup')
@@ -11,6 +13,8 @@ class CalTest(unittest.TestCase):
         print('clean up')
         del self.cal
 
+    # @unittest.skip('skip')
+    @unittest.skipIf(release_name=='lesson', 'skip!!')
     def test_add_num_and_double(self):
         self.assertEqual(self.cal.add_num_and_double(1,1), 4)
 
